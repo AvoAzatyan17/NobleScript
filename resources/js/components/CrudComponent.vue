b80fabc1d8b04e19c2639fff07d09c04c40d7e6f
AvoAzatyan17 <avetis.azatyan17@gmail.com>
2021-10-15 04:31:59 +0400

    project

---
+++ resources/js/components/CrudComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>crud</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CrudComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/IndexComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="header-title">
+            <h1>Noble Task</h1>
+        </div>
+        <b-card-group deck>
+            <b-card header="CRUD" header-tag="header">
+                <b-card-text>Make new crud action</b-card-text>
+                    <b-button  variant="primary">Go CRUD</b-button>
+            </b-card>
+            <b-card header="Tic Tac Toe" header-tag="header">
+                <b-card-text>Go game page and play</b-card-text>
+                <b-button href="/home" variant="success">Go Game</b-button>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
